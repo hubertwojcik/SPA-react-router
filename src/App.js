27e214e393307f171775b34db5428960e7a28bc8
Hubert Wojcik <hwojcikk@gmail.com>
2020-03-28 08:47:45 +0100

    install react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/App.css";
 
 function App() {
-	return <div>Hello</div>;
+	return (
+		<Router>
+			<div>Hello</div>
+		</Router>
+	);
 }
 
 export default App;
