8d214f74c2af2d9d3f9b0b7f03c3d9b9ad88d3e7
Hubert Wojcik <hwojcikk@gmail.com>
2020-03-31 16:24:36 +0200

    create contact page

--- src/layouts/Contact.js
+++ src/layouts/Contact.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import PageWrapper from "../components/PageWrapper";
+import Footer from "./Footer";
+
+import InputRadio from "../components/InputRadio";
+import Form from "../components/Form";
 
 import "../styles/Contact.scss";
 const FormSection = () => {
 	return (
 		<PageWrapper>
-			<div className="form">FormSection</div>
+			<div className="contact">
+				<div className="contact__form">
+					<Form />
+					{/* <form className="form"> */}
+					{/* <h2 className="form__heading">Contact</h2> */}
+					{/*
+					 */}
+					{/* <textarea name="textarea"></textarea>
+						<div className="form__radio-container">
+							<InputRadio name="gender" type="radio" sex="Man" />
+							<InputRadio name="gender" type="radio" sex="Woman" />
+						</div>
+					
+					</form> */}
+				</div>
+				<Footer />
+			</div>
 		</PageWrapper>
 	);
 };
