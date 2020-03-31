a99aeb9054c9522327e7123ee0ef6035e5a464bf
Hubert Wojcik <hwojcikk@gmail.com>
2020-03-31 16:24:45 +0200

    add button nad h1 to header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../styles/Header.scss";
 import PageWrapper from "../components/PageWrapper";
+import Button from "../components/Button";
+import BgVideo from "../components/BgVideo";
+import backgroundVideo from "../images/forest.mp4";
 
 const Header = props => {
 	return (
 		<PageWrapper>
-			<div className="header">Header</div>
+			<div className="header">
+				<div className="bg-video">
+					<BgVideo className="bg-video__content" src={backgroundVideo} />
+				</div>
+
+				<div className="header-container">
+					<h1 className="header__heading">Nature is Cool!</h1>
+					<Button type="submit" text="Welcome" className="header__button" />
+				</div>
+			</div>
 		</PageWrapper>
 	);
 };
