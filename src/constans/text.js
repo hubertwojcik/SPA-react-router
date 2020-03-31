b1987e2a953fa7cb055b012f664c8ad36768d682
Hubert Wojcik <hwojcikk@gmail.com>
2020-03-31 16:25:02 +0200

    create object with texts

---
+++ src/constans/text.js
@@ -0,0 +1,7 @@
+export default {
+	content:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur facere earum natus unde mollitia nemo quam, aperiam iusto quae sint optio labore. Voluptatum, est repellendus. Dicta ratione laudantium voluptate dolore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sed repellat harum iste. Eius accusamus molestiae, sint ex aut sed vitae deserunt est nesciunt quasi repellendus non ut. Quasi, repellendus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque ipsa earum ex, amet optio magnam a quos at porro officia ad laborum id, impedit enim illum, quae possimus molestias inventore.sssss",
+
+	picture:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, itaque necessitatibus illum optio autem maiores, dolore aut porro consequuntur voluptate exercitationem nesciunt ipsum incidunt tempora, repellat doloremque ratione! Amet, illum?"
+};
