507b23935e4b1c3804294a0dbe90ed196985c781
Hubert Wojcik <hwojcikk@gmail.com>
2020-03-31 10:15:33 +0200

    create button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = props => {
+	return (
+		<button type={props.type} className="form__button">
+			{props.text}
+		</button>
+	);
+};
+
+export default Button;
