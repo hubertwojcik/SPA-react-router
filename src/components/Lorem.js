d99baef00814967349f64ec029509dfa1472dfe0
Hubert Wojcik <hwojcikk@gmail.com>
2020-03-31 10:15:41 +0200

    create lorem component

---
+++ src/components/Lorem.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Lorem = () => {
+	return (
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+			accusantium iusto, aliquid, corporis itaque atque, ipsam eaque ab ad
+			dignissimos consectetur voluptatibus voluptate. Excepturi illo labore
+			veniam, commodi ut sit?
+		</p>
+	);
+};
+
+export default Lorem;
